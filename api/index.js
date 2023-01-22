4de0bf106a6151819c21286ae6767f53b7aa4794
milkdue <321432896@qq.com>
2023-01-22 09:29:11 +0800

    header

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
  * @Author: 可以清心
  * @Description: 
  * @Date: 2023-01-21 21:43:31
- * @LastEditTime: 2023-01-21 23:16:57
+ * @LastEditTime: 2023-01-22 09:28:21
  * @FilePath: \github-contribute\api\index.js
  */
 const https = require("https");
@@ -10,14 +10,6 @@ const cheerio = require("cheerio");
 
 module.exports = (req, res) => {  
     const NAME = req.query.name;
-    // *.keyiqingxin.cn
-    //设置允许跨域的域名，*代表允许任意域名跨域
-    res.header("Access-Control-Allow-Origin","*");
-    //允许的header类型
-    res.header("Access-Control-Allow-Headers","content-type");
-    //跨域允许的请求方式 
-    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-
     let html = "";
 
     https.get(`https://github.com/${NAME}`, function(resp){
